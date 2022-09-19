18f9b501e98f4d20495b6badf1e4837a1668db3a
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-09-19 11:06:39 -0300

    ej 1 2 y 3

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-
 const express = require('express');
 const app = express();
-const port = 9000;
+const port = 8080;
+const menu = require('./menu.json');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/menu', (req, res) => {
+  res.send(menu);
+});
+
+app.get('/menu/:id', (req, res) => {
+  const menuId = req.params.id;
+  const result = menu.find(menuItem => menuItem.id == menuId);
+  res.send(result);
+});
+
+app.get('/principales', (req, res) => {
+  const principales = menu.filter((item) =>
+    item.tipo == "principal"
+  )
+  res.send(principales)
+});
+
+app.get('/postre', (req, res) => {
+  const principales = menu.filter((item) =>
+    item.tipo == "principal"
+  )
+  res.send(principales)
 });
 
 app.listen(port, () => {
